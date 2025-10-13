87ba0b085e3a46095b3ffb2c513060103526924b
BeMe <187439614+soloflare1@users.noreply.github.com>
2025-10-12 23:56:38 -0700

    Create Operators_in_where.sql

---
+++ lab 3/Operators_in_where.sql
@@ -0,0 +1,10 @@
+
+                                                            # Visiting operators in where
+
+Arithmetic     :  +,  -,  *,  /,  %
+Comparison  :  =,  !=,  >,  <,  >=
+Logical           :  AND, OR, NOT, IN, BETWEEN, ALL, LIKE, ANY
+BITWISE        :  &, |
+
+Original data change -> update,set
+
