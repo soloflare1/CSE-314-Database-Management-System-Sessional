7f6ef86f50046619e9bff8fcb8dcc8bff3b2aa40
soloflare <187439614+soloflare1@users.noreply.github.com>
2025-12-05 01:22:55 +0600

    Create practice.sql

---
+++ lab-8/practice.sql
@@ -0,0 +1,103 @@
+DROP DATABASE IF EXISTS university;
+CREATE DATABASE university;
+USE university;
+
+CREATE TABLE students(
+     student_id INT PRIMARY KEY,
+     name VARCHAR(50),
+     dept VARCHAR(50)
+);
+
+CREATE TABLE courses(
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100),
+    credit INT CHECK (credit BETWEEN 1 AND 3)
+);
+
+CREATE TABLE enrollments(
+     student_id INT,
+     course_id INT,
+     grade CHAR(2),
+     score INT,
+     PRIMARY KEY (student_id,course_id),
+     
+     FOREIGN KEY (student_id)
+     REFERENCES students(student_id),
+     
+     FOREIGN KEY (course_id)
+     REFERENCES courses(course_id)
+);
+
+INSERT INTO students
+VALUES
+(1, 'Jiangly',"CSE"),
+(2, 'tourist', "EEE"),
+(3, 'Petr', "CSE"),
+(4, 'Benq',"CSE"),
+(5, 'Errichto',"BBA");
+
+INSERT INTO courses
+VALUES
+(101, 'DBMS', 3),
+(102, 'DSA', 3),
+(103, 'OS', 3);
+
+INSERT INTO enrollments
+VALUES
+(1, 101, 'A+', 95),
+(1, 102, 'A', 90),
+(2, 103, 'B+', 82),
+(3, 101, 'A', 93),
+(4, 103, 'B', 75),
+(5, 101, 'A', 89);
+
+
+-- 1. Show the total number of enrollments in the system.
+SELECT COUNT(student_id) as total_enrollments
+FROM enrollments;
+
+-- 2. Find the highest score per course.
+SELECT course_id, MAX(score) as highest_score
+FROM enrollments
+GROUP BY course_id;
+
+-- 3. Count how many students are in each department.
+SELECT dept, COUNT(student_id)
+FROM students
+GROUP BY dept;
+
+  
+-- 4. Show only those departments that have more than 1 student.
+SELECT dept, COUNT(student_id) as total_students
+FROM students
+GROUP BY dept
+HAVING COUNT(student_id) > 1;
+
+
+-- 5. Find students who scored above the average score in their course.
+SELECT 
+    s.name, 
+    e.course_id, 
+    e.score
+FROM students s
+JOIN enrollments e ON s.student_id = e.student_id
+WHERE e.score > (
+        SELECT AVG(score)
+        FROM enrollments
+        WHERE course_id = e.course_id
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
